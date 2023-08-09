38d0fbfa383853294503b34c9177836aa7c9860f
hialdev <al.developer12@gmail.com>
2023-08-09 11:04:30 +0700

    frontpage --item fix freeze header on scroll

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,13 @@ $('#menu-close').click(function (){
 $('#menu-open').click(function (){
     $('.menu').addClass('active');
 })
+
+var elementPosition = $('.menu-box').offset();
+
+$(window).scroll(function(){
+        if($(window).scrollTop() > elementPosition.top){
+              $('.menu-box').addClass('sticky-al');
+        } else {
+            $('.menu-box').removeClass('sticky-al');
+        }    
+});
