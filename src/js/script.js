2838f7d9f3396eefeb31881da89d6870bc7c1db8
hialdev <al.developer12@gmail.com>
2023-08-10 17:30:01 +0700

    add menu dropdown

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,16 @@ $('#menu-open').click(function (){
     $('.menu').addClass('active');
 })
 
+$('#menu-product-btn').click(function(){
+    $('#menu-product-dropdown').fadeToggle();
+})
+
 var elementPosition = $('.menu-box').offset();
 
 $(window).scroll(function(){
-        if($(window).scrollTop() > elementPosition.top){
-              $('.menu-box').addClass('sticky-al');
-        } else {
-            $('.menu-box').removeClass('sticky-al');
-        }    
+    if($(window).scrollTop() > elementPosition.top){
+            $('.menu-box').addClass('sticky-al');
+    } else {
+        $('.menu-box').removeClass('sticky-al');
+    }    
 });
