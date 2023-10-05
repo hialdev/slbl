82360f851861612910ca94d71ff5dda8d44c6d0f
hialdev <al.developer12@gmail.com>
2023-10-05 23:42:45 +0700

    finish --add share button

--- resources/views/partials/share.blade.php
+++ resources/views/partials/share.blade.php
@@ -2,7 +2,7 @@
     <div class="container py-5">
         <div class="fs-3">Beritahu yang Lain</div>
         <div class="d-flex align-items-center gap-2 flex-wrap">
-            <a target="_blank" href="{{$shareLinks['whatsapp']}}" class="d-flex align-items-center justify-content-center p-3 px-4 btn btn-outline-danger btn-lg rounded border border-2"><span class="iconify" data-icon="bi:whatsapp"></span></a>
+            <a target="_blank" href="{{$shareLinks['whatsapp']}}" class="d-flex align-items-center justify-content-center p-3 px-4 btn btn-outline-danger btn-lg rounded border border-2" data-action="share/whatsapp/share"><span class="iconify" data-icon="bi:whatsapp"></span></a>
             <a target="_blank" href="{{$shareLinks['facebook']}}" class="d-flex align-items-center justify-content-center p-3 px-4 btn btn-outline-danger btn-lg rounded border border-2"><span class="iconify" data-icon="bi:facebook"></span></a>
             <a target="_blank" href="{{$shareLinks['telegram']}}" class="d-flex align-items-center justify-content-center p-3 px-4 btn btn-outline-danger btn-lg rounded border border-2"><span class="iconify" data-icon="bi:telegram"></span></a>
             <a target="_blank" href="{{$shareLinks['linkedin']}}" class="d-flex align-items-center justify-content-center p-3 px-4 btn btn-outline-danger btn-lg rounded border border-2"><span class="iconify" data-icon="bi:linkedin"></span></a>
